224f57a65f9711144438cb7ca88092b93acf3e69
ayrton <ayrton.parkinson1@gmail.com>
2024-08-04 13:09:46 -0400

    Initial commit

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Ayrton Parkinson on 2024/08/04.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
